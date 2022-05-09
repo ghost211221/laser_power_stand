8eacf2782fcd0bc766e9b2005cf0427f304e757e
ghost211221 <solovyev.21@yandex.ru>
2022-05-09 21:58:43 +0300

    started devices

---
+++ src/core/devices/itla5300.py
@@ -0,0 +1,12 @@
+from src.core.devices.abstract import AbstractDevice
+
+
+class ITLA5300(AbstractDevice):
+
+    connetion_types = ['com']
+    dev_name = 'ITLA5300'
+
+    def __init__(self):
+        pass
+
+    
